84473d9197796e5ea4d87ddd7059c3a1c241cd0c
tiger <wanhu88@gmail.com>
2016-06-22 09:58:03 +0800

    新增单独引用模板等
    修改模板引用参数

--- astro-js-tpl.js
+++ astro-js-tpl.js
@@ -7,7 +7,6 @@ var file = require('file-plus');
 var nodeUtil = require('util');
 
 var util = require('lang-utils');
-
 module.exports = new astro.Middleware({
     modType: 'page',
     fileType: 'js'
@@ -19,14 +18,52 @@ module.exports = new astro.Middleware({
         next(asset);
         return;
     }
-    if (!asset.jsLibs || !asset.jsLibs[1].length) {
-        next(asset);
-        return;
-    }
-    let jsLibs = asset.jsLibs[1],
+    // let matches;
+    // if (!asset.prjCfg['_astro-js-tpl']) {
+    //     matches = {
+    //         names: []
+    //     };
+
+    //     matches.regexp = new RegExp(js_tpl.replace(/\{([^}]*)\}/ig, function(a, b) {
+    //         matches.names.push(b)
+    //         return '(.*?)';
+    //     }).replace(/[$.()^\[\]]/ig, function(a) {
+    //         return '\\' + a;
+    //     }), 'ig');
+    //     asset.prjCfg['_astro-js-tpl'] = matches;
+    // } else {
+    //     matches = asset.prjCfg['_astro-js-tpl']
+    // }
+
+
+
+    let tpls = [],
+        self = this,
+        jsLibs = asset.jsLibs[1],
         project = asset.project,
-        tpls = '',
-        jslib_assets = [];
+        imported = {},      // 已引用的模板
+        manutalTpl = [],    // 手动引用的模板
+        tpl_assets = [];    // 模板asset对象
+
+    asset.data.replace(/\$tpl_require\('(.*?)'\)/ig, function(a, b) {
+        manutalTpl.push(b);
+    });
+
+    if (manutalTpl.length) {
+        manutalTpl.forEach(function(h) {
+            let filePath = path.join(asset.prjCfg.jsCom, h + '.' + asset.prjCfg.htmlExt);
+            if (!fs.existsSync(filePath)) {
+                return;
+            } else {
+                tpl_assets.push(new astro.Asset({
+                    modType: 'jsCom',
+                    name: h,
+                    fileType: 'html',
+                    filePath: filePath
+                }));
+            }
+        });
+    }
 
     jsLibs.forEach(function(cpt) {
         var filePath = path.join(asset.prjCfg.jsCom, cpt);
@@ -39,35 +76,44 @@ module.exports = new astro.Middleware({
 
             var a = new astro.Asset({
                 modType: 'jsCom',
-                name: cpt,
+                name: cpt+'/'+ path.basename(file, '.' + asset.prjCfg.htmlExt),
                 fileType: 'html',
             });
-            a.path = file;
-            jslib_assets.push(a)
+            a.filePath = file;
+            tpl_assets.push(a)
         });
     });
 
-    astro.Asset.getContents(jslib_assets, function(asts) {
-        asts.forEach(function(a) {
-            var content = a.data;
-            if (content) {
-                content = content.trim();
-                content = content.replace(/\s*\n\s*/g, '');
-                content = JSON.stringify(content)
-                    .replace(/^['"]|['"]$/g, '')
-                    .replace(/'/g, '\\\'');
-                var fileName = path.basename(a.filePath, '.' + asset.prjCfg.htmlExt);
-                //var com = `$res.${js}.${fileName}=${content};\n`;
+    if (tpl_assets.length) {
+        astro.Asset.getContents(tpl_assets, function(asts) {
+            asts.forEach(function(a) {
+                if(imported[a.name]){
+                    return
+                }
+                imported[a.name] = true;
+                var content = a.data;
+                if (content) {
+                    content = content.trim();
+                    content = content.replace(/\s*\n\s*/g, '');
+                    content = JSON.stringify(content)
+                        .replace(/^['"]|['"]$/g, '')
+                        .replace(/'/g, '\\\'');
+                    var fileName = path.basename(a.filePath, '.' + asset.prjCfg.htmlExt);
+                    //var com = `$res.${js}.${fileName}=${content};\n`;
 
-                tpls = js_tpl.replace(/\{name\}/g, a.name)
-                    .replace(/\{file\}/g, fileName)
-                    .replace(/\{content\}/g, content) + '\n' + tpls;
-            }
+                    tpls.push(js_tpl.replace(/\{name\}/g, a.name)
+                        // .replace(/\{file\}/g, fileName)
+                        .replace(/\{content\}/g, content));
+                }
+            });
+            tpls.push('');
+
+            asset.data = (self.config.define?self.config.define+'\n\n': '') + tpls.join(';\n')+ '\n' + asset.data;
+            next(asset);
+
         });
-
-    asset.data = tpls + asset.data;
-    next(asset);
-
-    })
+    } else {
+        next(asset);
+    }
 
 });
